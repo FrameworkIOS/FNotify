d2fedc9017864cfba02310eb3138b1567e54890e
krishpranav <krisna.pranav@gmail.com>
2022-01-09 18:51:31 +0530

    FNotify: main draw

---
+++ FNotify/FNotify.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+public class FNotify: UIView {
+    override public func draw(_ rect: CGRect) {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 8,height: 8))
+        mainShape.frame = rect
+        mainShape.path = path.cgPath
+        mainShape.shadowColor = UIColor.black.cgColor
+        mainShape.shadowOffset = CGSize(width: 1, height: 1)
+        mainShape.shadowRadius = 15
+        mainShape.shadowOpacity = 0.4
+    }
+    
+    private var mainShape = CAShapeLayer()
+}
