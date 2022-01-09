16bd8e141865ce2d2329816335036a42dc14484c
krishpranav <krisna.pranav@gmail.com>
2022-01-09 18:47:16 +0530

    add files via upload

---
+++ FNotify/FNotify.h
@@ -0,0 +1,18 @@
+//
+//  FNotify.h
+//  FNotify
+//
+//  Created by Apple on 09/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FNotify.
+FOUNDATION_EXPORT double FNotifyVersionNumber;
+
+//! Project version string for FNotify.
+FOUNDATION_EXPORT const unsigned char FNotifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FNotify/PublicHeader.h>
+
+
