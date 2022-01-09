f0639d6281a03a393c1c2ee737a419f484ad95b3
krishpranav <krisna.pranav@gmail.com>
2022-01-09 19:14:48 +0530

    Example: FNotifyExample

---
+++ Example/FNotifyExample/FNotifyExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FNotifyExample
+//
+//  Created by Apple on 09/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/FNotifyExample/FNotifyExample/FNotifyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FNotifyExampleApp.swift
+//  FNotifyExample
+//
+//  Created by Apple on 09/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FNotifyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
