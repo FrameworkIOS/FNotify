d4986765ac811b32436c76dc3763dc0c925e8cdf
krishpranav <krisna.pranav@gmail.com>
2022-01-09 19:10:23 +0530

    FNotify

--- FNotify/FNotify.swift
+++ FNotify/FNotify.swift
@@ -79,6 +79,81 @@ public class FNotify: UIView {
         self.init(title: attrTitle, message: attrMessage, duration: duration, position: position, status: status, config: config)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(){
+        backgroundColor = UIColor.clear
+        
+        mainView.frame = bounds
+        mainView.backgroundColor = UIColor.clear
+        mainView.layer.addSublayer(mainShape)
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mainView)
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: topAnchor),
+            mainView.leftAnchor.constraint(equalTo: leftAnchor),
+            mainView.rightAnchor.constraint(equalTo: rightAnchor),
+            mainView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            mainView.widthAnchor.constraint(equalToConstant: min(400, UIScreen.main.bounds.width - 32)),
+            ])
+        
+        
+        iconView = UIImageView()
+        iconView.contentMode = .center
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.setContentHuggingPriority(.required, for: .horizontal)
+        mainView.addSubview(iconView)
+        NSLayoutConstraint.activate([
+            iconView.widthAnchor.constraint(equalToConstant: 30),
+            iconView.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
+            iconView.leftAnchor.constraint(equalTo: mainView.leftAnchor, constant: 8),
+            iconView.topAnchor.constraint(equalTo: mainView.topAnchor),
+            iconView.bottomAnchor.constraint(equalTo: mainView.bottomAnchor),
+            ])
+        
+        title.numberOfLines = 0
+        title.font = config.titleFont
+        title.textColor = config.titleColor
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.setContentHuggingPriority(.fittingSizeLevel, for: .horizontal)
+        title.setContentHuggingPriority(.required, for: .vertical)
+        mainView.addSubview(title)
+        NSLayoutConstraint.activate([
+            title.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 16),
+            title.leftAnchor.constraint(equalTo: iconView.rightAnchor, constant: 16),
+            title.rightAnchor.constraint(equalTo: mainView.rightAnchor, constant: -16),
+            ])
+        
+        message.numberOfLines = 0
+        message.font = config.messageFont
+        message.textColor = config.messageColor
+        message.translatesAutoresizingMaskIntoConstraints = false
+        message.setContentHuggingPriority(.fittingSizeLevel, for: .vertical)
+        mainView.addSubview(message)
+        NSLayoutConstraint.activate([
+            message.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 0),
+            message.leftAnchor.constraint(equalTo: title.leftAnchor, constant: 0),
+            message.rightAnchor.constraint(equalTo: title.rightAnchor, constant: 0),
+            message.bottomAnchor.constraint(equalTo: message.superview!.bottomAnchor, constant: -16),
+            ])
+        
+        mainShape.fillColor = config.backgrounColors[status]?.cgColor
+        iconView.image = config.images[status]
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.hideMessages))
+        isUserInteractionEnabled = true
+        addGestureRecognizer(tap)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.onMoving(pan:)))
+        addGestureRecognizer(pan)
+        
+        if let duration = duration {
+            timer = Timer.scheduledTimer(timeInterval: duration, target: self, selector: #selector(self.hideMessages), userInfo: nil, repeats: false)
+        }
+    }
+    
 }
 
 
