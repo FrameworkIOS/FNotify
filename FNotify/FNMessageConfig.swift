da54b19a36536a471f0e5ac7d07955701cc8587a
krishpranav <krisna.pranav@gmail.com>
2022-01-09 19:02:05 +0530

    FNotify

--- FNotify/FNMessageConfig.swift
+++ FNotify/FNMessageConfig.swift
@@ -5,4 +5,14 @@
 //  Created by Krisna Pranav on 09/01/22.
 //
 
-import Foundation
+import UIKit
+
+public struct FNMessageConfig {
+    public static var shared = FNMessageConfig()
+    
+    public var titleFont : UIFont = UIFont.boldSystemFont(ofSize: 15)
+    public var titleColor : UIColor = .white
+    public messageColor = UIColor = .white
+    public var messageFont : UIFont = UIFont.systemFont(ofSize: 14)
+    
+}
